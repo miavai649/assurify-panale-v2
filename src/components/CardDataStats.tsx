418a5939508273b92b8b03d8235db112dad3092b
miavai649 <mahmudulhaquenoor@gmail.com>
2025-01-27 16:10:23 +0600

    refactor[dashboard, carDataStats]: data stats card modified

--- src/components/CardDataStats.tsx
+++ src/components/CardDataStats.tsx
@@ -34,7 +34,7 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
         <span
           className={`flex items-center gap-1 text-sm font-medium ${
             levelUp && 'text-meta-3'
-          } ${levelDown && 'text-meta-5'} `}
+          } ${levelDown && 'text-meta-1'} `}
         >
           {rate}
 
@@ -55,7 +55,7 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
           )}
           {levelDown && (
             <svg
-              className="fill-meta-5"
+              className="fill-meta-1"
               width="10"
               height="11"
               viewBox="0 0 10 11"
