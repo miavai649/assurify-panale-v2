d45789b09797d84d7ba0a74f5785272bf59fdf97
miavai649 <mahmudulhaquenoor@gmail.com>
2025-02-13 13:10:10 +0600

    feat: showing dummy stores data in a table

---
+++ src/lib/formatDate.ts
@@ -0,0 +1,7 @@
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+};
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,5 +1,72 @@
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { TableColumnsType, Tag } from 'antd';
+import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+import CustomTable from '../../components/Tables/CustomTable';
+import { formatDate } from '../../lib/formatDate';
+import { shopifyStores } from './storesDummyData';
+
 const Stores = () => {
-  return <div>Stores</div>;
+  // promotions table column
+  const columns: TableColumnsType<any> = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      sorter: (a, b) => a.id - b.id,
+    },
+    {
+      title: 'Store Name',
+      dataIndex: 'storeName',
+    },
+    {
+      title: 'Current Plan',
+      dataIndex: 'currentPlan',
+    },
+    {
+      title: 'App Status',
+      dataIndex: 'appStatus',
+      render: (appStatus: string) => {
+        if (appStatus === 'Installed') {
+          return (
+            <Tag icon={<CheckCircleOutlined />} color="success">
+              Installed
+            </Tag>
+          );
+        } else {
+          return (
+            <Tag icon={<CloseCircleOutlined />} color="error">
+              Uninstalled
+            </Tag>
+          );
+        }
+      },
+      filters: [
+        {
+          text: 'Installed',
+          value: 'Installed',
+        },
+        {
+          text: 'Uninstalled',
+          value: 'Uninstalled',
+        },
+      ],
+      onFilter: (value, record) => {
+        return record.appStatus === value;
+      },
+    },
+    {
+      title: 'Installed At',
+      dataIndex: 'installedAt',
+      render: (installedAt: string) => formatDate(installedAt),
+    },
+  ];
+
+  return (
+    <div>
+      <Breadcrumb pageName="Installation Request" />
+
+      <CustomTable columns={columns} tableSize="large" data={shopifyStores} />
+    </div>
+  );
 };
 
 export default Stores;
